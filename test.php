16fc8e0e3e985af5a15eaf8b390b850d497368ca
Ne-Pridumal <73481785+Ne-Pridumal@users.noreply.github.com>
2020-11-22 15:08:10 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,30 @@
+<?php
+require "read.php";
+function testDir(){
+	$listTest = ["dirs" => [],"files" => []];
+	$direction = opendir(readInput("Ссылка на директорию"));
+	while ($element = readdir($direction)){
+		if(in_array($element,[".",".."])){
+		continue;
+		}
+		if (is_dir($element))
+		{
+			$listTest["dirs"][$element] = [
+				"is_readable" => is_readable($element),
+				"is_writable" => is_writable($element)
+			];
+		}
+		else
+		{
+			$listTest["files"][$element] = [
+				"is_readable" => is_readable($element),
+				"is_writable" => is_writable($element),
+				"size" => filesize($element),
+			];
+		}
+	}
+	closedir($direction);
+	print_r($listTest);
+}
+testDir();
+
